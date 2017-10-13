7d52d5366cfe23cef2371ff9391253ef26245d37
lachrist <lachrist@vub.ac.be>
2017-10-13 15:36:17 +0200

    bug on raw sandbox

--- raw/sandbox.js
+++ raw/sandbox.js
@@ -1,11 +1,12 @@
 
 const Fs = require("fs");
+const Path = require("path");
 
 module.exports = (path, options, callback) => {
   Fs.readFile(path, "utf8", (error, content) => {
     callback(error, {
       type: "raw",
-      path: "/"+Path.relative(options.basedir||__dirname, path),
+      path: "/"+Path.relative(options.basedir||Path.dirname(path), path),
       content: content
     });
   });
